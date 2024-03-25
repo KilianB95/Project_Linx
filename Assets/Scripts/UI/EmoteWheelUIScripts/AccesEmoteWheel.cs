168ec5ce21fca1d7880341281ec1f535581250db
rickdohmen <70955143+rickdohmen@users.noreply.github.com>
2024-03-25 10:53:13 +0100

    added onClick funtion in script
    onclick voor buttons n de script toegevoegd,
    buttons toegevoegd in de canvas

--- Assets/Scripts/UI/EmoteWheelUIScripts/AccesEmoteWheel.cs
+++ Assets/Scripts/UI/EmoteWheelUIScripts/AccesEmoteWheel.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AccesEmoteWheel : MonoBehaviour
 {
     [SerializeField] private GameObject _emoteWheel;
+    private GameObject[] _emoteObjects;
+    private Transform _transform;
     [SerializeField] private KeyCode _emoteWheelKeyCode;
 
     private void Update()
@@ -31,5 +31,20 @@ public class AccesEmoteWheel : MonoBehaviour
         _emoteWheel.SetActive(false);
     }
 
+    private void TriggerEmote(int indexEmote)
+    {
+        foreach (GameObject _emoteObject in _emoteObjects)
+        {
+            _emoteObject.SetActive(false);
+        }
+
+        if(indexEmote >= 0 && indexEmote < _emoteObjects.Length)
+        {
+            _emoteObjects[indexEmote].SetActive(true);
+            _emoteObjects[indexEmote].transform.position = _transform.position + Vector3.up * 2f;
+        }
+    }
+
+    private void OnEmoteButtonClick(int indexEmote) => TriggerEmote(indexEmote);
 
 }
