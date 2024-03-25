c7f59bde390de4b6af98d280fe1b80b04f0b4c17
KilianB95 <kilianbultje@outlook.com>
2024-03-25 11:48:13 +0100

    Basic Ai Klaar
    Basic Ai klaar
    Enemy kan Chasen
    Enemy zoek een pad en blijft patrols
    uitvoeren

--- Assets/Scripts/Enemy_AI/Patrolling_Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy_AI/Patrolling_Enemy/EnemyPatrol.cs
@@ -11,25 +11,53 @@ public class EnemyPatrol : MonoBehaviour
 
     [SerializeField] private LayerMask _groundLayer, _playerLayer;
 
+    //Patrol State
     private Vector3 _destinationPoint;
     private bool _walkingPoint;
     [SerializeField] private float _range;
+
+    //State Change
+    [SerializeField] float _sightRange, _attackRange;
+    private bool _playerInSight, _inAttackRange;
+
     void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _player = GameObject.Find("Player");
+        _player = GameObject.FindWithTag("Player");
     }
 
     private void Update()
     {
-        Patrol();
+        _playerInSight = Physics.CheckSphere(transform.position,_sightRange, _playerLayer);
+        _inAttackRange = Physics.CheckSphere(transform.position, _attackRange, _playerLayer);
+        _agent.SetDestination(_player.transform.position);
+
+        if(!_playerInSight && !_inAttackRange) 
+            Patrol();
+        if(_playerInSight && !_inAttackRange) 
+            Chase();
+        if(!_playerInSight && !_inAttackRange)
+            Attack();
+    }
+
+    private void Chase()
+    {
+        _agent.SetDestination(_player.transform.position);
+    }
+
+    private void Attack()
+    {
+        _agent.SetDestination(transform.position);
     }
 
     private void Patrol()
     {
-        if (!_walkingPoint) SearchDestination();
-        if (_walkingPoint) _agent.SetDestination(_destinationPoint);
-        if(Vector3.Distance(transform.position, _destinationPoint) < 10) _walkingPoint = false;
+        if (!_walkingPoint) 
+            SearchDestination();
+        if (_walkingPoint) 
+            _agent.SetDestination(_destinationPoint);
+        if(Vector3.Distance(transform.position, _destinationPoint) < 10) 
+            _walkingPoint = false;
     }
 
     private void SearchDestination()
