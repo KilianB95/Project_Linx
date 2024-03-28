0b0259aad901b68a4e5254c6ee8b716647b60787
KilianB95 <kilianbultje@outlook.com>
2024-03-28 14:17:16 +0100

    Friendly AI basis gemaakt
    AI Friendly basis gemaakt
    Zou uiteindelijke getweaked moeten worden.
    
    Om de Ai beter te maken

--- Assets/Scripts/Friendly_AI/FriendlyAi.cs
+++ Assets/Scripts/Friendly_AI/FriendlyAi.cs
@@ -7,6 +7,8 @@ public class FriendlyAi : MonoBehaviour
 {
     private GameObject _player;
     private NavMeshAgent _agent;
+    private float _xOffset, _zOffset, _timeUntilPosChange;
+    [SerializeField] private float _changePositionTimer;
 
     [SerializeField] private LayerMask _groundLayer, _playerLayer;
 
@@ -17,21 +19,31 @@ public class FriendlyAi : MonoBehaviour
     {
         _agent = GetComponent<NavMeshAgent>();
         _player = GameObject.FindWithTag("Player");
+        _changePositionTimer = Random.Range(4, 9);
     }
 
     private void Update()
     {
+        _timeUntilPosChange += Time.deltaTime;
+
         _playerFollow = Physics.CheckSphere(transform.position, _sightRange, _playerLayer);
         _inRange = Physics.CheckSphere(transform.position, _followRange, _playerLayer);
         _agent.SetDestination(_player.transform.position);
 
-        if(_playerFollow && !_inRange)
+        if (_playerFollow && _inRange)
             FollowPlayer();
 
     }
 
     private void FollowPlayer()
     {
-        _agent.SetDestination(_player.transform.position);
+        if (_timeUntilPosChange > _changePositionTimer) 
+        {
+            _xOffset = Random.Range(-5.0f, 5.0f);
+            _zOffset = Random.Range(1.5f, 5f);
+            _timeUntilPosChange = 0;
+        }
+        _agent.destination = new Vector3(_player.transform.position.x +_xOffset, _player.transform.position.y, _player.transform.position.z +_zOffset);
+ 
     }
 }
